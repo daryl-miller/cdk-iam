8843686687409ad850ee587c9ae3eba388386ea6
Daryl Miller <daryl.miller@mondo.com.au>
2023-01-27 23:40:37 +1100

    Add user role for k8 dns updates.

--- lib/cdk-iam-stack.ts
+++ lib/cdk-iam-stack.ts
@@ -1,16 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
+import { SecretValue } from 'aws-cdk-lib';
+import { AccessKey, Effect, PolicyStatement, User } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager'
 export class CdkIamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const route53GetChangePolicy = new PolicyStatement({
+      actions: ['route53:GetChange'],
+      effect: Effect.ALLOW,
+      resources: ['arn:aws:route53:::change/*']
+    })
+    const route53ChangeResourceRecordSetsPolicy = new PolicyStatement({
+      actions: [        
+        'route53:ChangeResourceRecordSets',
+        'route53:ListResourceRecordSets'],
+      effect: Effect.ALLOW,
+      resources: ['arn:aws:route53:::hostedzone/*']
+    })
+    const route53ListHostedZonesByNamePolicy = new PolicyStatement({
+      actions: ['route53:ListHostedZonesByName'],
+      effect: Effect.ALLOW,
+      resources: ['*']
+    })
+    const user = new User(this, 'kubernetesDNSCertManagerUser');
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkIamQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    user.addToPolicy(route53GetChangePolicy)
+    user.addToPolicy(route53ChangeResourceRecordSetsPolicy)
+    user.addToPolicy(route53ListHostedZonesByNamePolicy)
   }
 }
