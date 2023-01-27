469a7dedd00c5bd0c2a8d91c4d8c3e43e05f98a0
Daryl Miller <daryl.miller@mondo.com.au>
2023-01-27 23:48:44 +1100

    add dns manager role

--- lib/cdk-iam-stack.ts
+++ lib/cdk-iam-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { SecretValue } from 'aws-cdk-lib';
-import { AccessKey, Effect, PolicyStatement, User } from 'aws-cdk-lib/aws-iam';
+import { AccessKey, Effect, PolicyStatement, Role, User } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager'
 export class CdkIamStack extends cdk.Stack {
@@ -26,8 +26,12 @@ export class CdkIamStack extends cdk.Stack {
     })
     const user = new User(this, 'kubernetesDNSCertManagerUser');
 
-    user.addToPolicy(route53GetChangePolicy)
-    user.addToPolicy(route53ChangeResourceRecordSetsPolicy)
-    user.addToPolicy(route53ListHostedZonesByNamePolicy)
+    const dnsMangerRole = new Role(this, 'kubernetesDNSCertManagerRole', {
+      assumedBy: user
+    })
+
+    dnsMangerRole.addToPolicy(route53GetChangePolicy)
+    dnsMangerRole.addToPolicy(route53ChangeResourceRecordSetsPolicy)
+    dnsMangerRole.addToPolicy(route53ListHostedZonesByNamePolicy)
   }
 }
